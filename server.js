4c9c148c3167cd14bab068c0a2090a4fbac5b15c
Shadi Mahmoud <131858501+shadi77777@users.noreply.github.com>
2024-05-21 19:48:37 +0200

    sad

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.use(express.static('.')); // Serverer statiske filer fra rodmappen
-
-app.listen(port, () => {
-    console.log(`Server lytter på http://localhost:${port}`);
-});
